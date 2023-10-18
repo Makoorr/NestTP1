25f9c613787c7993dae72d7366b65c8c1a8e5592
Makoorr <azizbenrejeb@gmail.com>
2023-10-18 20:04:38 +0100

    validation centralisé

--- src/todo/entities/todo.entity/todo.entity.ts
+++ src/todo/entities/todo.entity/todo.entity.ts
@@ -1,5 +1,6 @@
 import { TodoStatusEnum } from 'src/enums/todo-status.enum';
 import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn, VersionColumn } from 'typeorm';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 @Entity('todo')
 export class TodoEntity {
@@ -7,9 +8,20 @@ export class TodoEntity {
     id: string;
 
     @Column()
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3, {
+        message: 'Nom trop court.'
+    })
+    @MaxLength(10)
     name: string;
-
+    
     @Column()
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(10, {
+        message: 'Description trop courte.'
+    })
     description: string;
     
     @Column({
