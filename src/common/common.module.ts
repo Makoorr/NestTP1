1814667deb024e228155fb69be5cf0c354213068
Makoorr <azizbenrejeb@gmail.com>
2023-10-19 15:17:46 +0100

    updated common

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,11 +1,18 @@
 import { Global, Module } from '@nestjs/common';
 import { CommonService } from './common.service';
+import { v4 as uuidv4 } from 'uuid'
 
 @Global()
 @Module({
     imports: [],
     controllers: [],
-    providers: [CommonService],
+    providers: [
+        {
+            provide: 'UUID',
+            useValue: uuidv4,
+        },
+        CommonService
+    ],
     exports: [],
 })
 export class CommonModule {
